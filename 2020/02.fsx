b4974d6c3d223ab8dea8790dbf5a1f8e2cdf3e27
Oliver Fleckenstein <oliverfl@live.dk>
2020-12-02 13:55:51 +0800

    2020/02 cleanup

--- 2020/02.fsx
+++ 2020/02.fsx
@@ -2,41 +2,39 @@
 #r "../src/AdventOfCode/bin/Debug/net5.0/AdventOfCode.dll"
 
 open AdventOfCode.Core
-open AdventOfCode.Utils
 
 let data = input 2020 2
-// let data = """1-3 a: abcde
-// 1-3 b: cdefg
-// 2-9 c: ccccccccc"""
 
 let count c (str: string) =
     str |> Seq.filter (fun x -> x = c) |> Seq.length
 
 let toRange (r: string) =
-    let [ s; e ] =
-        r.Split("-") |> Seq.map int |> Seq.toList
+    match r.Split("-") |> Seq.map int |> Seq.toList with
+    | s :: e :: _ -> (s, e)
+    | _ -> failwith "Unable to parse range"
 
-    (s, e)
-
-let countFilter ((s, e): (int * int)) c = s <= c && c <= e
+let toTuple =
+    function
+    | [| r; c; s |] -> (toRange r, Seq.head c, s)
+    | _ -> failwith "unexpected number of elements in line"
 
 let parse (d: string) =
     d.Trim().Split("\n")
-    |> Seq.map (fun s -> s.Split(" "))
-    |> Seq.map (fun [| r; c; s |] -> (toRange r, Seq.head c, s))
+    |> Seq.map ((fun (str: string) -> str.Split " ") >> toTuple)
+
+// Level 1
+let validate ((s, e), c, str) =
+    count c str |> (fun n -> s <= n && n <= e)
 
 let a =
-    parse data
-    |> Seq.filter (fun (r, c, str) -> countFilter r <| count c str)
-    |> Seq.length
+    parse data |> Seq.filter validate |> Seq.length
 
 submit 2020 2 Level.One a
 
-let validate2 ((s, e): int * int) c (str: string) = (str.[s - 1] = c) <> (str.[e - 1] = c)
+// Level 2
+let validate2 (((i, j): int * int), c, (str: string)) = (str.[i - 1] = c) <> (str.[j - 1] = c)
 
 let b =
-    parse data
-    |> Seq.filter (fun (r, c, str) -> validate2 r c str)
-    |> Seq.length
+    parse data |> Seq.filter validate2 |> Seq.length
 
 submit 2020 2 Level.Two b
