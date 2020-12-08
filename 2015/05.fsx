ffcd0530fc2c5b6baf2b69b35fc1500545003183
Oliver Fleckenstein <oliverfl@live.dk>
2020-12-08 21:30:46 +0100

    Solution for 2015 day 5 and 6

---
+++ 2015/05.fsx
@@ -0,0 +1,58 @@
+#r "nuget: FSharp.Data"
+#r "../src/AdventOfCode/bin/Debug/net5.0/AdventOfCode.dll"
+
+open AdventOfCode.Core
+open AdventOfCode.Utils
+open System.Text.RegularExpressions
+
+let data = input 2015 5
+let testData = testInput 2015 5
+
+let parse = splitLines
+
+let hasRepeat =
+    Seq.pairwise >> Seq.exists (fun (a, b) -> a = b)
+
+let isVowel (c: char) = "aeiou".Contains(c)
+
+let hasThreeVowels (str: string) =
+    str |> Seq.filter isVowel |> Seq.length >= 3
+
+let badWords = [ "ab"; "cd"; "pq"; "xy" ]
+
+let noBadWords (str: string) =
+    not
+    <| Seq.exists (fun (word: string) -> str.Contains(word)) badWords
+
+let isNice str =
+    let filters =
+        [ hasRepeat
+          hasThreeVowels
+          noBadWords ]
+
+    Seq.forall (fun filter -> filter str) filters
+
+testData
+|> (splitLines
+    >> Seq.map isNice
+    >> Seq.iter (printfn "%b"))
+
+let solve = parse >> Seq.filter isNice >> Seq.length
+
+submit 2015 5 Level.One <| solve data
+
+// Level 2
+let hasPair str = Regex.IsMatch(str, @"(..).*\1")
+
+let hasRepeat2 =
+    Seq.windowed 3
+    >> Seq.exists (function
+        | [| a; _; b |] -> a = b
+        | _ -> false)
+
+let isNice2 str = hasPair str && hasRepeat2 str
+
+let solve2 =
+    parse >> Seq.filter isNice2 >> Seq.length
+
+submit 2015 5 Level.Two <| solve2 data
---
+++ 2015/06.fsx
@@ -0,0 +1,69 @@
+#r "nuget: FSharp.Data"
+#r "../src/AdventOfCode/bin/Debug/net5.0/AdventOfCode.dll"
+
+open AdventOfCode.Core
+open AdventOfCode.Utils
+
+let data = input 2015 6
+
+let parsePoint (str: string) =
+    match str.Split(",") with
+    | [| x; y |] -> (int x, int y)
+    | _ -> failwith "Unable to parse point"
+
+let parseLine =
+    function
+    | ReMatch @"(turn on|toggle|turn off) (\d+,\d+) through (\d+,\d+)" [ _; cmd; s; e ] ->
+        (cmd.Value, parsePoint s.Value, parsePoint e.Value)
+    | _ -> failwith "Could not parse"
+
+let parse = splitLines >> Seq.map parseLine
+
+type Command = string * (int * int) * (int * int)
+
+let modifyState cmd =
+    match cmd with
+    | "turn on" -> fun _ -> true
+    | "turn off" -> fun _ -> false
+    | "toggle" -> fun b -> not b
+    | _ -> failwith $"Unknown command {cmd}"
+
+let update modify (lights: 'a [,]) ((cmd, (x0, y0), (x1, y1)): Command) =
+    let m = modify cmd
+    for x = min x0 x1 to max x0 x1 do
+        for y = min y0 y1 to max y0 y1 do
+            lights.[x, y] <- m lights.[x, y]
+
+
+let follow init modify (inst: seq<Command>) =
+    let mutable lights = Array2D.init 1000 1000 (fun _ _ -> init)
+    Seq.iter (update modify lights) inst
+    lights
+
+let countBool (grid: bool [,]) =
+    let mutable c = 0
+    Array2D.iter (fun b -> if b then c <- c + 1) grid
+    c
+
+let solve =
+    parse >> follow false modifyState >> countBool
+
+submit 2015 6 Level.One <| solve data
+
+// Level 2
+let modifyBrightness cmd =
+    match cmd with
+    | "turn on" -> fun x -> x + 1
+    | "turn off" -> fun x -> max 0 (x - 1)
+    | "toggle" -> fun x -> x + 2
+    | _ -> failwith $"Unknown command {cmd}"
+
+let countInt (grid: int [,]) =
+    let mutable c = 0
+    Array2D.iter (fun x -> c <- c + x) grid
+    c
+
+let solve2 =
+    parse >> follow 0 modifyBrightness >> countInt
+
+submit 2015 6 Level.Two <| solve2 data
