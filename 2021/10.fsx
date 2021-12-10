468017902f1d05427f505bb316c96aea513fe9fe
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-10 20:28:11 +0100

    2021/10 solution for part B

--- 2021/10.fsx
+++ 2021/10.fsx
@@ -17,31 +17,88 @@ let points c =
     | '>' -> 25137
     | _ -> failwith $"Unable to get point for '{c}'"
 
-let solve (input: string list) =
-    let closing (c: char) =
-        c = '}' || c = ')' || c = ']' || c = '>'
 
-    let opposite c =
-        match c with
-        | '{' -> '}'
-        | '(' -> ')'
-        | '[' -> ']'
-        | '<' -> '>'
-        | _ -> failwith $"Unsupported character '{c}'"
+type Either<'a, 'b> =
+    | Left of 'a
+    | Right of 'b
 
-    let rec helper parsed rest =
+let getRight elements =
+    elements
+    |> Seq.filter (fun x ->
+        match x with
+        | Left _ -> false
+        | Right _ -> true)
+    |> Seq.map (fun x ->
+        match x with
+        | Right value -> value
+        | _ -> failwith "Unsupported")
+
+let getLeft elements =
+    elements
+    |> Seq.filter (fun x ->
+        match x with
+        | Left _ -> true
+        | Right _ -> false)
+    |> Seq.map (fun x ->
+        match x with
+        | Left value -> value
+        | _ -> failwith "Unsupported")
+
+let closing (c: char) =
+    c = '}' || c = ')' || c = ']' || c = '>'
+
+let opposite c =
+    match c with
+    | '{' -> '}'
+    | '(' -> ')'
+    | '[' -> ']'
+    | '<' -> '>'
+    | _ -> failwith $"Unsupported character '{c}'"
+
+let analyze str =
+    let rec analyze' parsed rest =
         match parsed, rest with
-        | _, [] -> None
-        | p :: _, n :: _ when closing n && opposite p <> n -> Some n
-        | _ :: parsed', n :: rest' when closing n -> helper parsed' rest'
-        | _, n :: rest' -> helper (n :: parsed) rest'
+        | _, [] -> Left <| Seq.map opposite parsed
+        | p :: _, n :: _ when closing n && opposite p <> n -> Right n
+        | _ :: parsed', n :: rest' when closing n -> analyze' parsed' rest'
+        | _, n :: rest' -> analyze' (n :: parsed) rest'
 
-    input
-    |> List.map (List.ofSeq >> helper [])
-    |> filterOption
-    |> Seq.sumBy points
+    analyze' [] str
+
+let solve =
+    List.map (List.ofSeq >> analyze)
+    >> getRight
+    >> Seq.sumBy points
 
 let solver = parse >> solve
 
 testSolution Level.One 26397 <| solver testData
 submit 2021 10 Level.One <| solver data
+
+// Part B
+let points' c =
+    match c with
+    | ')' -> 1
+    | ']' -> 2
+    | '}' -> 3
+    | '>' -> 4
+    | _ -> failwith "Unsupported point"
+
+let solve' input =
+    let values =
+        input
+        |> (List.map (List.ofSeq >> analyze)
+            >> getLeft
+            >> Seq.map (
+                Seq.map (points' >> int64)
+                >> Seq.reduce (fun acc v -> acc * 5L + v)
+            )
+            >> List.ofSeq
+            >> List.sort)
+
+    values.[values.Length / 2]
+
+let solver' = parse >> solve'
+
+testSolution Level.Two 288957L <| solver' testData
+submit 2021 10 Level.Two <| solver' data
