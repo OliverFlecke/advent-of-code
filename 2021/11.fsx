85a382bbd5c5031687838a43a7443f82a4a4a870
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-11 11:19:02 +0100

    2021/11 solution for part B

--- 2021/11.fsx
+++ 2021/11.fsx
@@ -22,33 +22,29 @@ let neighbors map p =
         && 0 <= y
         && y < Array2D.length2 map)
 
+let iter grid =
+    let rec run grid =
+        let next =
+            grid
+            |> Array2D.mapi (fun x y v ->
+                if v = 0 || v > 9 then
+                    0
+                else
+                    let neighboringFlashes =
+                        neighbors grid (x, y)
+                        |> Seq.map (uncurry <| Array2D.get grid)
+                        |> count 10
+
+                    min 10 <| v + neighboringFlashes)
+
+        if next = grid then next else run next
+
+    grid |> Array2D.map ((+) 1) |> run
+
+let numberOfFlashes =
+    Seq.cast<int> >> Seq.filter ((=) 0) >> Seq.length
+
 let solve amount input =
-    let step1 = Array2D.map ((+) 1)
-
-    let step2 grid =
-        let rec run grid =
-            let next =
-                grid
-                |> Array2D.mapi (fun x y v ->
-                    if v = 0 || v > 9 then
-                        0
-                    else
-                        let neighboringFlashes =
-                            neighbors grid (x, y)
-                            |> Seq.map (uncurry <| Array2D.get grid)
-                            |> count 10
-
-                        min 10 <| v + neighboringFlashes)
-
-            if next = grid then next else run next
-
-        run grid
-
-    let numberOfFlashes =
-        Seq.cast<int> >> Seq.filter ((=) 0) >> Seq.length
-
-    let iter = step1 >> step2
-
     Seq.fold
         (fun (flashes, grid) _ ->
             let next = iter grid
@@ -62,3 +58,14 @@ let solver = parse >> solve 100
 
 testSolution Level.One 1656 <| solver testData
 submit 2021 11 Level.One <| solver data
+
+// Part B
+let solve' grid =
+    Seq.scan (fun grid _ -> iter grid) grid (Seq.initInfinite id)
+    |> Seq.takeWhile (Seq.cast<int> >> Seq.exists ((<>) 0))
+    |> Seq.length
+
+let solver' = parse >> solve'
+
+testSolution Level.Two 195 <| solver' testData
+submit 2021 11 Level.Two <| solver' data
