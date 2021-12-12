f23c32b7a5c6434b101ee622418b463d572d1b07
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-12 12:23:16 +0100

    2021/12 solution for part B. Not optimal

--- 2021/12.fsx
+++ 2021/12.fsx
@@ -57,6 +57,7 @@ let parse =
     >> Seq.fold addEdge Map.empty
 
 let isUpper (str: string) = Seq.forall (System.Char.IsUpper) str
+let isLower str = isUpper str |> not
 
 let solve (graph: Graph) =
     let bfs start =
@@ -97,3 +98,55 @@ testSolution Level.One 19 <| solver mediumTest
 testSolution Level.One 226 <| solver largeTest
 
 submit 2021 12 Level.One <| solver data
+
+// Part B
+let solve' graph =
+    let bfs start =
+        let rec bfs' twice (paths: string list list) visited (frontier: string list list) =
+            match frontier with
+            | [] -> paths
+            | path :: frontier' ->
+                match path with
+                | "end" :: _ as path -> bfs' twice (path :: paths) visited frontier'
+                | node :: _ as path ->
+                    let neighbors = neighbors graph node
+
+                    neighbors
+                    |> Seq.filter (flip Set.contains visited >> not)
+                    |> Seq.map (fun n ->
+                        let front = (n :: path) :: frontier'
+
+                        if isUpper n || n = "start" || n = "end" then
+                            seq { bfs' twice paths visited front }
+                        else
+                            seq {
+                                if twice then
+                                    yield bfs' twice paths (Set.add n visited) front
+                                else
+                                    yield bfs' true paths visited front
+                                    yield bfs' false paths (Set.add n visited) front
+                            })
+                    |> Seq.collect id
+                    |> Seq.collect id
+                    |> List.ofSeq
+
+                | _ -> failwith $"Unable to handle path {path}"
+
+        bfs' false [] (set [ start ]) [ [ start ] ]
+
+
+    let paths = bfs "start"
+
+    // paths
+    // |> Seq.map (Seq.rev >> String.concat ",")
+    // |> Seq.sort
+    // |> Seq.iter print
+
+    paths |> Seq.distinct |> Seq.length
+
+let solver' = parse >> solve'
+
+testSolution Level.One 36 <| solver' testData
+testSolution Level.One 103 <| solver' mediumTest
+testSolution Level.One 3509 <| solver' largeTest
+submit 2021 12 Level.Two <| solver' data
