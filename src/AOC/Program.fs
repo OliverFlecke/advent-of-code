fc47eb153c65e7a9d24ee068571b67c1174ab0f7
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-12 18:30:34 +0100

    Added timing of results

--- src/AOC/Program.fs
+++ src/AOC/Program.fs
@@ -4,6 +4,8 @@ open System
 open System.Reflection
 open AdventOfCode
 
+open System.Diagnostics
+
 let solutions =
     Assembly.GetExecutingAssembly().GetTypes()
     |> Seq.filter (fun x ->
@@ -11,9 +13,59 @@ let solutions =
         |> Seq.exists (fun i -> i = typeof<ISolution>))
     |> Seq.map (fun t -> Activator.CreateInstance(t) :?> ISolution)
 
-for s in solutions do
-    printfn "Running for %i day %i" s.year s.day
-    let data = input s.year s.day
-    let a = s.solveA data
+type Timed = { result: string; time: int64 }
 
-    submit s.year s.day Level.One a |> ignore
+let timeit f : Timed =
+    let timer = new Stopwatch()
+    timer.Start()
+    let res = f ()
+    timer.Stop()
+
+    { result = res
+      time = timer.ElapsedMilliseconds }
+
+type TimedResult =
+    { year: int
+      day: int
+      a: Timed
+      b: Timed
+      parseTime: int64 }
+
+let execute (s: ISolution) : TimedResult =
+    let parsed = timeit (fun () -> input s.year s.day)
+
+    let a =
+        timeit (fun () -> s.solveA parsed.result)
+
+    let b =
+        timeit (fun () -> s.solveB parsed.result)
+
+    { year = s.year
+      day = s.day
+      a = a
+      b = b
+      parseTime = parsed.time }
+
+let results = solutions |> Seq.map execute
+
+let printTable results =
+    for result in results do
+        printfn
+            "Solution %i/%2i | Part A: %10s %6i ms | Part B: %10s %6i ms | Parsing %10i ms  Total: %10i ms"
+            result.year
+            result.day
+            result.a.result
+            result.a.time
+            result.b.result
+            result.b.time
+            result.parseTime
+            (result.a.time + result.b.time + result.parseTime)
+
+do printTable results
+
+let doSubmit = true
+
+if doSubmit then
+    for r in results do
+        submit r.year r.day Level.One r.a.result |> ignore
+        submit r.year r.day Level.Two r.b.result |> ignore
