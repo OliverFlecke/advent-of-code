f4036627a1f581484c0e000d7ccb8349c014d2de
Oliver Fleckenstein <oliverfl@live.dk>
2021-12-15 22:27:40 +0100

    chore: migrated solution for 2021/5

--- 2021/05.fsx
+++ 2021/05.fsx
@@ -7,17 +7,7 @@ open AdventOfCode.Utils
 
 let data = input 2021 5
 
-let testData =
-    "0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2"
+let testData = testInput 2021 5
 
 type Line = (int * int) * (int * int)
 
