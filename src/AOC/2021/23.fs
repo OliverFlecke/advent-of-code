5dd97bcae96ad028e01ef4640c5e15043c5df281
Oliver Fleckenstein <oliverfl@live.dk>
2022-11-10 21:20:04 +0000

    wip: Solutions for 2021 day 23

--- src/AOC/2021/23.fs
+++ src/AOC/2021/23.fs
@@ -72,13 +72,6 @@ module private Day23 =
             SparseMatrix.empty
             validPositions
 
-    // let amphipodsInColumn state col : (Position * Amphipod) seq =
-    //     state
-    //     |> Map.filter (fun p _ -> fst p = col)
-    //     |> Map.filter (fun _ a -> Option.isSome a)
-    //     |> Map.map (fun p a -> Option.get a)
-    //     |> Map.toSeq
-
     let isAtGoal state a (x, y) =
         let belowIsAtGoal =
             Map.tryFind (x, y + 1) state
@@ -93,25 +86,15 @@ module private Day23 =
         isAtGoal state a dst
         || (snd from <> 1
             && not <| List.contains (fst dst) columns)
-    // if snd from <> 1 then
-    //     not <| List.contains (fst dst) columns
-    // else
-    //     Amphipod.target a = fst dst
-    //     && (snd dst = 3
-    //         || (snd dst = 2
-    //             && (Map.tryFind (fst dst, 3) state
-    //                 |> Option.filter (Option.isSome)
-    //                 |> Option.map (Option.get >> ((=) a))
-    //                 |> Option.defaultValue false)))
 
     let add (a: Position) (b: Position) = (fst a + fst b), (snd a + snd b)
 
-    let cost (state: State) =
+    let stateCost (state: State) =
         state
         |> Map.toSeq
         |> Seq.filter (snd >> Option.isSome)
         |> Seq.map (fun (p, a) -> p, Option.get a)
-        |> Seq.sumBy (fun (p, a) -> Amphipod.distToTarget a p)
+        |> Seq.sumBy (fun (p, a) -> Amphipod.distToTarget a p * Amphipod.energy a)
 
     let solve (state: SparseMatrix<Amphipod option>) =
         let ps = Set.ofSeq validPositions
@@ -134,14 +117,9 @@ module private Day23 =
             let rec search visited frontier result =
                 match PriorityQ.popMin frontier with
                 | None -> result
-                // result
-                // |> Map.toSeq
-                // |> Seq.tryFind (fun (p, _) -> isAtGoal state amphipod p)
-                // |> Option.map (fun x -> Map.ofSeq (seq { x }))
-                // |> Option.defaultValue result
                 // If amphipod can move directly to its goal, throw away all other states
                 | Some ((cost, pos), _) when isAtGoal state amphipod pos ->
-                    printfn $"Found move to goal for {amphipod} from {start} to {pos}"
+                    // printfn $"Found move to goal for {amphipod} from {start} to {pos}"
                     Map.ofSeq [ (pos, cost) ]
                 | Some ((cost, pos), frontier') ->
                     let visited' = Set.add pos visited
@@ -150,7 +128,6 @@ module private Day23 =
                         if pos <> start && canStop state amphipod start pos then
                             Map.add pos cost result
                         else
-                            // print $"{amphipod}@{start} -> Can't stop at {pos}"
                             result
 
                     let frontier'' =
@@ -175,12 +152,10 @@ module private Day23 =
                 let state' = Map.add pos None state
 
                 search_for_actions a state' pos
-                |> Map.map (fun pos _ -> Amphipod.distToTarget a pos)
-                // |> Map.map (fun pos cost ->
-                //     (cost + Amphipod.distToTarget a pos)
-                //     * Amphipod.energy a)
                 |> Map.toSeq
-                |> Seq.map (fun (p, c) -> c, Map.add p (Some a) state'))
+                |> Seq.map (fun (p, c) ->
+                    let cost = Amphipod.distToTarget a pos + c
+                    (cost, c * Amphipod.energy a), Map.add p (Some a) state'))
             |> Seq.concat
 
         // state
@@ -191,30 +166,43 @@ module private Day23 =
         let rec helper visited frontier =
             match PriorityQ.popMin frontier with
             | None -> failwith "No solution"
-            | Some ((cost, state), _) when isSolved state -> cost, state
-            | Some ((cost, state), frontier) ->
-                let visited = Set.add state visited
+            | Some ((_, (state, tail, energy)), _) when isSolved state ->
+                tail
+                |> Seq.rev
+                |> Seq.iter (fun (s, e) ->
+                    printfn $"Cost: {e}"
+                    printState s)
 
-                printf "Cost: %d\n" cost
-                printState state
-                printfn ""
+                printfn $"States visited: {Set.count visited}"
+                energy, state
+            | Some ((cost, (state, tail, energy)), frontier) ->
+                let visited' = Set.add state visited
+                // if Set.count visited' % 1000 = 0 then
+                //     printfn $"Visited {Set.count visited'} states. Current cost {energy}"
+
+                // printf "Cost: %d\n" cost
+                // printState state
+                // printfn ""
 
                 let frontier' =
                     nextStates state
-                    |> Seq.filter (snd >> flip Set.contains visited >> not)
-                    |> Seq.fold (fun f (c, s) -> PriorityQ.push (cost + c) s f) frontier
+                    |> Seq.filter (snd >> flip Set.contains visited' >> not)
+                    |> Seq.fold
+                        (fun f ((c, e), s) ->
+                            PriorityQ.push (e + stateCost state) (s, (s, energy + e) :: tail, energy + e) f)
+                        frontier
 
                 // frontier'
                 // |> PriorityQ.toSeq
                 // |> Seq.map snd
                 // |> Seq.iter printState
-                helper visited frontier'
+                helper visited' frontier'
 
         let (cost, state) =
-            helper Set.empty (PriorityQ.singleton 0u state)
+            helper Set.empty (PriorityQ.singleton 0u (state, [], 0u))
 
-        printf $"Cost {cost}"
-        print state
+        printfn $"Cost {cost}"
+        // print state
 
         cost
 
@@ -233,8 +221,18 @@ type Year2021Day23() =
   #########"
                   ) ]
 
-        member _.testB = seq []
+        member _.testB =
+            seq [ Int 44169,
+                  Some(
+                      "#############
+#...........#
+###B#C#B#D###
+  #D#C#B#A#
+  #D#B#A#C#
+  #A#D#C#A#
+  #########"
+                  ) ]
 
         member _.solveA input = input |> parse |> solve |> int |> Int
 
-        member _.solveB input = Int 0
+        member _.solveB input = input |> parse |> solve |> int |> Int
